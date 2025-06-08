627bf9330913583dd8fb0058b8cd3c20ea313cd4
George Dickeson <george.dickeson@gmail.com>
2025-06-08 21:20:50 +0930

    Fixed default route

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,9 +1,14 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '@/App.vue'
+import { encodePres } from '@/utils/share'
+
+const defaultPres = encodePres({ g: ['a', 'b'], r:['aaa', 'bb', 'abababab']})
 
 const routes = [
+  { path: '/', redirect: `/g/${defaultPres}` },
+  { path: '/g', redirect: `/g/${defaultPres}` },
   { path: '/g/:pres?', name: 'group', component: Home },
-  { path: '/:pathMatch(.*)*', redirect: '/g/' },
+  { path: '/:pathMatch(.*)*', redirect: '/' },
 ]
 
 export const router = createRouter({
